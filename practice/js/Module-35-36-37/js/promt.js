228cba3c76b8fb99e9126de902721ac540eac3b0
nayemhasan45 <nayemhasan75907@gmail.com>
2022-09-07 12:23:30 -0700

    learning alert and promt

---
+++ practice/js/Module-35-36-37/js/promt.js
@@ -0,0 +1,14 @@
+console.log('clicked');
+const modalBtn = () => {
+    const random = Math.random() * 10;
+    console.log(random);
+    if (random > 5) {
+
+        alert('kire boro number dicish kan');
+    }
+
+}
+
+const promtBtn = () => {
+    prompt('what is your name');
+}
