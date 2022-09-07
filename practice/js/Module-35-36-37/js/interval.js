e4b193c34f6fadf214cf4b8a8e443df2c8246329
nayemhasan45 <nayemhasan75907@gmail.com>
2022-09-07 09:02:36 -0700

    settimeout and setinterval parctice done

--- practice/js/Module-35-36-37/js/interval.js
+++ practice/js/Module-35-36-37/js/interval.js
@@ -1,1 +1,25 @@
-console.log('hi');
+// console.log('hi');
+function kaka() {
+    console.log('third');
+}
+const kaka2 = () => {
+    console.log('third from arrow function');
+}
+console.log('first');
+console.log('second');
+// console.log('third')
+// setTimeout(kaka, 1000)
+// setTimeout(() => {
+//     kaka();
+// }, 2000);
+// 
+let seconds = 0;
+const intervalId = setInterval(() => {
+    console.log(++seconds);
+    if (seconds === 5) {
+        clearInterval(intervalId);
+    }
+}, 1000);
+// kaka2();
+console.log('fourth');
+console.log('fifth');
