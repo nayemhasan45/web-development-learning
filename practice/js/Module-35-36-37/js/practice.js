d295162b461fe337261f942be60b00e8e2ff0cb8
nayemhasan45 <nayemhasan75907@gmail.com>
2022-09-13 11:55:37 -0700

    all adding to git

---
+++ practice/js/Module-35-36-37/js/practice.js
@@ -0,0 +1,10 @@
+const num = [45, 33, 2, 345, 65, 75, 87, 86, 43, 23, 45, 55];
+const num2 = [...num];
+num.push(34);
+const number = num;
+// console.log(number);
+// number.push(600);
+// console.log(num);
+num2.push(5555);
+console.log(num);
+// console.log(num2)
--- practice/js/Module-35-36-37/parctice.js
+++ practice/js/Module-35-36-37/parctice.js
@@ -5,3 +5,28 @@ let str = '';
 //     console.log(false);
 // }
 console.log(str);
+
+const marks = 60;
+const resubmit = -5;
+// console.log(!(marks > 0 || resubmit > 0));
+
+
+const person = { name: "hero", id: 101, address: "BD", postalCode: 40321 };
+// console.log(Object.keys(person).length);
+// const animals = ['cat', 'dog', 'rat']; console.log(animals.includes('Cat'));
+const array = [2, 3, 4, 5, 2];
+// const unique = array =>
+// array.indexOf(“21”);
+
+const add = (para1, para2) =>
+    para2 + para1;
+// function add(x, y) {
+//     return x + y;
+// }
+console.log(add('21', 3));
+
+// const b = 6, b = 6
+// console.log(b);
+
+const colors = { mango: 'green', grapes: 'black', organe: 'yellow' };
+console.log(colors[grapes])
