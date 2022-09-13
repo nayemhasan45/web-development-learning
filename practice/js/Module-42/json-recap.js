f3952706a54bc5e94d9999ebc13e89839df634aa
nayemhasan45 <nayemhasan75907@gmail.com>
2022-09-13 11:09:04 -0700

    pushing all think up to date

--- practice/js/Module-42/json-recap.js
+++ practice/js/Module-42/json-recap.js
@@ -16,3 +16,22 @@ const newStudent = { ...student.course };
 console.log(newStudent);
 // const remaining = newStudent.filter(course => course.name !== 'ged');
 // console.log(remaining);
+
+const name = 'Hero'; const age = 34;
+// const person = { name, age };
+// console.log(person);
+
+// const person2 = { name: 'ddd' }
+// console.log(person2.name);
+// console.log(person2["name"]);
+// console.log(person2[name]);
+
+// const adventurer = { name: 'Alice', cat: { name: 'Lucy' } };
+// const ratName = adventurer?.rat?.name;
+// console.log(ratName)
+
+// let person = null;
+// console.log(person ? person : "person is null");
+
+const obj = { a: 1 };
+console.log(Object.keys(obj).length === 0);
